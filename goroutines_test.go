ceff81d1baab2aabb7e47ac76e4f3797215f097d
Abdul Ghofur <abdulghofur.me@gmail.com>
2022-08-13 03:33:12 +0700

    Run 100.000 Goroutines

--- goroutines_test.go
+++ goroutines_test.go
@@ -17,3 +17,14 @@ func TestCreateGoroutine(t *testing.T) {
 
 	time.Sleep(time.Second * 1)
 }
+
+func DisplayNumber(number int) {
+	fmt.Println("Display", number)
+}
+
+func TestManyGoroutines(t *testing.T) {
+	for i := 0; i <= 100000; i++ {
+		go DisplayNumber(i)
+	}
+	time.Sleep(5 * time.Second)
+}
